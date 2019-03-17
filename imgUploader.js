9591bc8417437382637effb0512c81fa734d5252
skywalker512 <houzhenhong@hotmail.com>
2019-03-17 18:49:52 +0800

    [提交] 清理测试

--- imgUploader.js
+++ imgUploader.js
@@ -14,8 +14,7 @@ export default async function fileUploader(body) {
     const dataBuffer = new Buffer.from(base64Data, 'base64');
     const _path = pathStr()
     await mkdirp(_path)
-    const res = await fs.writeFileSync(path.join(_path, `${Math.random().toString(36).substr(2,6)}_${filename}`), dataBuffer)
-    console.log(res)
+    await fs.writeFileSync(path.join(_path, `${Math.random().toString(36).substr(2,6)}_${filename}`), dataBuffer)
     return true
   } catch (error) {
     console.log(error)
